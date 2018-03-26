fe3f851388e136ecce5a0c114939489ea60d13d9
muharif <muhammad.arif@scilifelab.se>
2018-03-26 20:03:14 +0200

    remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='files2xlsx',
-    version='0.1dev',
-    packages=['files2xlsx',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
